3407ef9f385147644fd514de2f9adc1b8268abc2
Pratham Jain <jainpratham2601@gmail.com>
2024-06-08 12:16:09 +0530

    Revert "Update authControllers.js"
    This reverts commit 2466e99b2c2adaa74108bf428ba33ad72645ca93.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -51,7 +51,7 @@ exports.signup = async (req, res) => {
         if (emailExists) {
           return response_400(res, "Email is already in use");
         }
-        verifyEmail(email);
+        verifyEmail();
         // Generate unique user ID (choose one approach)
 
         let userId;
