400b4e49e0d6ef073300fd78e9c69b41ac56dcdf
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-06-08 10:04:19 +0530

    Added email verification via 5 digit OTP

---
+++ middlewares/verifyEmail.middleware.js
@@ -0,0 +1,40 @@
+const nodemailer = require("nodemailer");
+const { response_400 } = require("../utils/responseCodes.utils");
+require("dotenv").config();
+exports.verifyEmail = async (req, res) => {
+    try {
+        const OTP = Math.floor(Math.random() * 90000) + 10000;
+        const s = `The OTP for email verification is ${OTP}`;
+        const auth = nodemailer.createTransport({
+          service: "gmail",
+          secure: true,
+          port: 465,
+          auth: {
+            user: process.env.SENDER_EMAIL,
+            pass: process.env.SENDER_PASSWORD,
+          },
+        });
+        const receiver = {
+          from: process.env.SENDER_EMAIL,
+          to: [email],
+          subject: "Email verification",
+          text: s,
+        };
+        auth.sendMail(receiver, (err, emailResponse) => {
+          if (err) {
+            throw err;
+          }
+          console.log("success");
+          res.end();
+        });
+        const enteredOTP = parseInt(req.body);
+        if (enteredOTP === OTP) {
+            res.redirect('/home');
+        }
+        else {
+            res.redirect('/signup').send('Invalid OTP');
+        }
+    } catch (error) {
+        return response_400(res, error);
+    }
+}
